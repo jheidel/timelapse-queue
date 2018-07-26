336373f6a4fccde6bd5281611226ff2e79ab1992
Jeff Heidel <jheidel@gmail.com>
2018-07-25 23:02:54 -0700

    Filebrowse incremental work.

--- filebrowse/filebrowse.go
+++ filebrowse/filebrowse.go
@@ -2,7 +2,12 @@ package filebrowse
 
 import (
 	"encoding/json"
+	"errors"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 type FileBrowser struct {
@@ -10,17 +15,76 @@ type FileBrowser struct {
 	Root string
 }
 
+type Directory struct {
+	Name string
+	Path string
+}
+
 type Timelapse struct {
 }
 
 type Response struct {
-	Parents    []string
-	DirNames   []string
+	Parents    []*Directory
+	Dirs       []*Directory
 	Timelapses []*Timelapse
 }
 
-func (f *FileBrowser) listPath(path string) (*Response, error) {
-	return nil, nil
+func (f *FileBrowser) listPath(p string) (*Response, error) {
+	root, err := filepath.EvalSymlinks(f.Root)
+	if err != nil {
+		return nil, err
+	}
+	b, err := filepath.EvalSymlinks(filepath.Join(root, p))
+	if err != nil {
+		return nil, err
+	}
+	if !strings.HasPrefix(b, root) {
+		return nil, errors.New("permission denied, not in root")
+	}
+
+	files, err := ioutil.ReadDir(b)
+	if err != nil {
+		return nil, err
+	}
+
+	r := &Response{}
+
+	// Generate list of directories
+	for _, finfo := range files {
+		rel, err := filepath.Rel(root, filepath.Join(b, finfo.Name()))
+		if err != nil {
+			return nil, err
+		}
+		if finfo.IsDir() {
+			d := &Directory{
+				Name: finfo.Name(),
+				Path: rel,
+			}
+			r.Dirs = append(r.Dirs, d)
+		}
+	}
+
+	// Generates list of parents
+	rel, err := filepath.Rel(root, b)
+	if err != nil {
+		return nil, err
+	}
+	pl := strings.Split(rel, string(os.PathSeparator))
+	r.Parents = append(r.Parents, &Directory{
+		Name: "[top]",
+		Path: ".",
+	})
+	for i := range pl {
+		d := &Directory{
+			Name: pl[i],
+			Path: filepath.Join(pl[:i+1]...),
+		}
+		if d.Path != "." {
+			r.Parents = append(r.Parents, d)
+		}
+	}
+
+	return r, nil
 }
 
 func (f *FileBrowser) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -29,10 +93,6 @@ func (f *FileBrowser) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	p := r.Form.Get("path")
-	if p == "" {
-		http.Error(w, "missing path", http.StatusBadRequest)
-		return
-	}
 	response, err := f.listPath(p)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
