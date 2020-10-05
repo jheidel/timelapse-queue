1e786bb52f0eb99b3c3c51bf16e02d1515fbcf63
Jeff Heidel <jheidel@gmail.com>
2020-10-04 21:48:06 -0700

    Use volume mount instead of bind to try and fix strange unbinding issue

--- docker-run.sh
+++ docker-run.sh
@@ -7,9 +7,10 @@ TIMELAPSE_DIR=/data/share/
 
 docker run \
   -d \
-  --restart unless-stopped \
+  --restart always \
   -p 8888:8080 \
-  --mount type=bind,source=${TIMELAPSE_DIR?},target=/mnt/fsroot --user 1002:1002 \
+  -v ${TIMELAPSE_DIR?}:/mnt/fsroot \
+  --user 1002:1002 \
   --cpus=5.5 --memory=20g --memory-swap=20g \
   --name timelapse-queue \
   jheidel/timelapse-queue
